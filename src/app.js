4b78e92950595ddbb0868fdd1b1729af001dd7df
kreetz <bronze.reach@gmail.com>
2025-04-03 22:56:55 +0300

    Task 02

---
+++ src/app.js
@@ -0,0 +1,23 @@
+import express from 'express';
+const app = express();
+const port = 3000;
+
+app.use('/public', express.static('public'));
+
+app.get('/api/v1/cat', (req, res) => {
+    console.log('query params object', req.query);
+    const myData = {
+        cat_id: 1,
+        name: 'Dog',
+        birthdate: '100 BC',
+        weight: 100,
+        owner: 'Pekka',
+        iamge: 'https://loremflickr.com/320/240/cat'
+
+    };
+    res.json(myData);
+});
+
+app.listen(port, () => {
+  console.log(`Server running`);
+});
